dff49bd38a5ca658233fa76a36d6a0d5d8f80297
Liam Tengelis <ltengelis@gmail.com>
2019-11-21 19:45:06 -0800

    provide jwt on successful sign-in

--- db_schema/def/functions/api.check_username_password.sql
+++ db_schema/def/functions/api.check_username_password.sql
@@ -3,25 +3,31 @@ CREATE FUNCTION api.check_username_password(
   IN p_username    TEXT,
   IN p_password    TEXT
 )
-RETURNS BOOLEAN
+RETURNS TEXT
 AS $$
 DECLARE
   v_pw_hash       api.ac_user.password_hash%TYPE;
+  v_user_id       api.ac_user.user_id%TYPE;
 BEGIN
   BEGIN
-    SELECT password_hash
-      INTO v_pw_hash
+    SELECT password_hash,
+           user_id
+      INTO v_pw_hash,
+           v_user_id
       FROM api.ac_user
      WHERE username = p_username
     ;
     EXCEPTION
       WHEN NO_DATA_FOUND THEN
-       RETURN FALSE;
+       RETURN '';
   END;
-  RETURN v_pw_hash = crypt(
-                       p_password,
-                       v_pw_hash
-                     );
+  IF v_pw_hash = crypt(
+                   p_password,
+                   v_pw_hash
+                 ) THEN
+    RETURN v_user_id::TEXT;
+  END IF;
+  RETURN '';
 END;
 $$
 LANGUAGE plpgsql
--- http_server/src/main.ts
+++ http_server/src/main.ts
@@ -1,10 +1,12 @@
 import { env } from 'process';
+import { readFileSync } from 'fs';
 
 import * as Koa  from 'koa';
 import * as KoaRouter from 'koa-router';
 import * as koaBodyParser from 'koa-bodyparser'
 
 import { http_get } from './lib/http_request';
+import { generate_jwt } from './lib/generate_jwt';
 
 
 const app = new Koa();
@@ -13,27 +15,37 @@ const router = new KoaRouter();
 const api_db_port = parseInt(env['API_DB_PORT']);
 const http_listen_port = parseInt(env['HTTP_LISTEN_PORT']);
 
+const private_key: Buffer = readFileSync('/srv/host_private_key');
+
 
 router
   .post('/authenticate/username-password', async (ctx, next) => {
     try {
       const username: string = ctx.request.body.username;
       const password: string = ctx.request.body.password;
-      const result = await http_get(
-        `http://postgrest:3001/rpc/check_username_password?p_username=${username}&p_password=${password}`
+      const user_id = await http_get(
+        `http://postgrest:${api_db_port}/rpc/check_username_password?p_username=${username}&p_password=${password}`
       );
-      if (result) {
-        ctx.body = 'SUCCESS';
+      if (user_id) {
+        const token = await generate_jwt(
+          user_id, private_key
+        );
+        ctx.body = {
+          token: token,
+        };
       } else {
         ctx.status = 401;
         ctx.body = 'FAILURE';
       }
     } catch (e) {
+      console.trace(e);
       ctx.status = 401;
       ctx.body = 'FAILURE';
     }
   })
 ;
+
+
 
 app
   .use(koaBodyParser())
