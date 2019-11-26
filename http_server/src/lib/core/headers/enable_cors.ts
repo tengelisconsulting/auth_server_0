f402c7b3b86b07db61bb0bdf6095a04c27f8ce81
liam@tengelisconsulting.com <liam@tengelisconsulting.com>
2019-11-25 23:36:02 -0800

    allow credentials

--- http_server/src/lib/core/headers/enable_cors.ts
+++ http_server/src/lib/core/headers/enable_cors.ts
@@ -3,6 +3,7 @@ import * as Koa from "koa";
 
 export function set_cors_headers(ctx: Koa.ParameterizedContext<any>): void {
   ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Credentials', 'true');
   ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
 }
