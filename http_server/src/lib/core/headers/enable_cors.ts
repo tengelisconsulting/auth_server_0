b0be73e085159c70a6891917b9592cc3add193ab
liam@tengelisconsulting.com <liam@tengelisconsulting.com>
2019-11-26 20:19:59 -0800

    finally getting response headers right for CORS auth

--- http_server/src/lib/core/headers/enable_cors.ts
+++ http_server/src/lib/core/headers/enable_cors.ts
@@ -2,7 +2,9 @@ import * as Koa from "koa";
 
 
 export function set_cors_headers(ctx: Koa.ParameterizedContext<any>): void {
-  ctx.set('Access-Control-Allow-Origin', '*');
+  const req_origin = ctx.request.get('Origin');
+  ctx.set('Access-Control-Allow-Origin', req_origin);
+  ctx.set('Access-Control-Allow-Credentials', 'true');
   ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
 }
